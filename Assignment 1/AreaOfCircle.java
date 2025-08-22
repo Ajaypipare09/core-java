e86eb4ff1f84000580e536fcd14242c6649f6d70
Ajay prakash pipare <144015485+Ajaypipare@users.noreply.github.com>
2025-08-22 21:54:16 +0530

    first commit

--- Assignment 1/AreaOfCircle.java
+++ Assignment 1/AreaOfCircle.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class AreaOfCircle {
     public static void main(String[] args) {
-        double r, area; // better use double for radius also
+        double r, area; 
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter Radius: ");
