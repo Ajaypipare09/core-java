936d506a4b1c89d2a23bc76b8bcb8e80cecd7289
Ajay prakash pipare <144015485+Ajaypipare@users.noreply.github.com>
2025-08-23 16:46:17 +0530

    first commit

---
+++ Assignment 2/Postive.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+    public class Postive {
+     
+     public static void main(String[] args){
+
+     Scanner sc = new Scanner(System.in);
+
+     System.out.print("Enter a Number :");
+     int n = sc.nextInt();
+
+    if(n > 0)
+    System.out.println("Postive number");
+   
+    else 
+      System.out.println("invalid");
+
+    }
+
+
+}
