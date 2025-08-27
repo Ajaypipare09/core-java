956765f595bd57f3e5965ce9bb169e4dccfc7c95
Ajay prakash pipare <144015485+Ajaypipare@users.noreply.github.com>
2025-08-27 11:53:51 +0530

    first commit

---
+++ Assignment 4/sumnumber.java
@@ -0,0 +1,14 @@
+ public class sumnumber{
+
+  public static void main(String[] args){
+
+    int sum = 0;
+   for(int i = 100; i<= 150; i++){
+    sum = sum + i;
+
+   System.out.println("sumnumber:"+ i + "=" + sum);
+     }
+
+   }
+
+}
