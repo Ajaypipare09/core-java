f58b1a752d95dac5f6ca980affff88f7f7fe0354
Ajay prakash pipare <144015485+Ajaypipare@users.noreply.github.com>
2025-08-27 23:07:18 +0530

    first commit

---
+++ Assignment 4/onetoNodd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class onetoNodd {
+
+   public static void main(String[] args){
+  
+    Scanner sc = new Scanner(System.in);
+
+   System.out.println("Enter a number:");
+    int n = sc.nextInt();
+
+     for(int i = 1; i<n; i+=2 ){
+
+     System.out.println(i);
+    }
+
+  }
+
+}
