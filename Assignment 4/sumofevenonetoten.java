a0fd8cdffa32b212932c2192f5e4a0881590bc96
Ajay prakash pipare <144015485+Ajaypipare@users.noreply.github.com>
2025-08-28 18:49:27 +0530

    first commit

---
+++ Assignment 4/sumofevenonetoten.java
@@ -0,0 +1,13 @@
+public class   sumofevenonetoten {
+
+   public static void main(String[] args){
+     int sum = 0;
+     for(int i = 2; i<=10; i+=2 ){
+     sum = sum + i;
+
+     System.out.println("sum of"+ i + "=" + sum);
+    }
+
+  }
+
+}
