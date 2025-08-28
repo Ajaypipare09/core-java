5999e0fdcc43ff1dd3e00e5cdd161f2cc35f057d
Ajay prakash pipare <144015485+Ajaypipare@users.noreply.github.com>
2025-08-28 18:52:51 +0530

    first commit

---
+++ Assignment 4/sumofoddonetoten.java
@@ -0,0 +1,13 @@
+public class   sumofoddonetoten {
+
+   public static void main(String[] args){
+     int sum = 0;
+     for(int i = 1; i<=10; i+=2 ){
+     sum = sum + i;
+
+     System.out.println("sum of"+ i + "=" + sum);
+    }
+
+  }
+
+}
