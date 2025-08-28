7cde2c171201cbe85ad13f0536f8b64e75585d70
Ajay prakash pipare <144015485+Ajaypipare@users.noreply.github.com>
2025-08-28 18:56:29 +0530

    first commit

---
+++ Assignment 4/sumofevenht.java
@@ -0,0 +1,13 @@
+public class    sumofevenht {
+
+   public static void main(String[] args){
+     int sum = 0;
+     for(int i = 100; i<=200; i+=2 ){
+     sum = sum + i;
+
+     System.out.println("sum of"+ i + "=" + sum);
+    }
+
+  }
+
+}
