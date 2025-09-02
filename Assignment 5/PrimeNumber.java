ce6e6652133be6b26e419dd72b32e8de6921a7c7
Ajay prakash pipare <144015485+Ajaypipare@users.noreply.github.com>
2025-09-02 19:01:06 +0530

    first commit

---
+++ Assignment 5/PrimeNumber.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+   public class PrimeNumber {
+
+   public static void main(String[] args){
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter a Number");
+    int n = sc.nextInt();
+  
+    System.out.println("Prime number between 1 and " + n + ":");
+
+     for(int i = 2; i<= n; i++) {
+   
+      boolean isPrime = true;
+
+     for(int j = 2; j * j <= i; j++) {
+
+      if (i % j == 0) {
+
+        isPrime = false;
+  
+       break;
+
+      }
+    }
+
+    if (isPrime) {
+         System.out.print(i + " ");
+
+         }
+ 
+     }
+}
+
+}
+    
